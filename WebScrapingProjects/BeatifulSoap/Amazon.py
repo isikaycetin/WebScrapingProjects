58e44c60be96d6bcba1326844a3073667416c2a2
IsikayCetin <121253342+isikaycetin@users.noreply.github.com>
2025-06-11 16:15:34 +0300

    Update Amazon.py

--- WebScrapingProjects/BeatifulSoap/Amazon.py
+++ WebScrapingProjects/BeatifulSoap/Amazon.py
@@ -9,19 +9,17 @@ url ='https://www.amazon.com.tr/s?k=laptop&__mk_tr_TR=%C3%85M%C3%85%C5%BD%C3%95%
 excel_datas= []
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive'
+# güvenlik nedeniyle devre dışı bıraktım.
 }
 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 
+# scraping işlemleri    
 def scraping(the_soup):
-    laptops = the_soup.find_all('div', class_='puis-card-container')
+    laptops = the_soup.find_all('div', class_='puis-card-container') # dış divi aldım laptoplar
     for i, laptop in enumerate(laptops):
-        laptop_link = laptop.find('a', class_='a-link-normal')
+        laptop_link = laptop.find('a', class_='a-link-normal')     # product lara gitmek için linkler
 
         if laptop_link:
             laptop_url = 'https://www.amazon.com.tr' + laptop_link.get('href')
@@ -29,15 +27,15 @@ def scraping(the_soup):
             detail_soup = BeautifulSoup(response.text, 'html.parser')
 
             # Marka
-            tr_mark = detail_soup.find('tr', class_='po-brand')
+            tr_mark = detail_soup.find('tr', class_='po-brand')  #markasını al
             if tr_mark:
                 td_tags = tr_mark.find_all('td')
-                td_mark = td_tags[1].text.strip() if len(td_tags) > 1 else "Marka eksik"
+                td_mark = td_tags[1].text.strip() if len(td_tags) > 1 else "Marka eksik"  # satırın karşısı boşsa eksik blgi var demek
             else:
                 td_mark = "Marka yok"
 
             # Renk
-            tr_color = detail_soup.find('tr', class_='po-color')
+            tr_color = detail_soup.find('tr', class_='po-color') # rengini al
             if tr_color:
                 td_tags = tr_color.find_all('td')
                 td_color = td_tags[1].text.strip() if len(td_tags) > 1 else "Renk eksik"
@@ -45,15 +43,16 @@ def scraping(the_soup):
                 td_color = "Renk yok"
 
             # RAM
-            tr_ram = detail_soup.find('tr', class_='po-ram_memory.installed_size')
+            tr_ram = detail_soup.find('tr', class_='po-ram_memory.installed_size')  # ram boyutunu al
             if tr_ram:
                 td_tags = tr_ram.find_all('td')
                 td_ram = td_tags[1].text.strip() if len(td_tags) > 1 else "RAM eksik"
             else:
                 td_ram = "RAM bilgisi yok"
 
-            print(f"{i+1}. {td_mark}  -  {td_color} -  {td_ram}")
-            excel_datas.append({
+            print(f"{i+1}. {td_mark}  -  {td_color} -  {td_ram}") 
+            
+            excel_datas.append({     # listeye kaydet
                 "Marka": td_mark,
                 "Renk": td_color,
                 "RAM": td_ram,
@@ -62,25 +61,25 @@ def scraping(the_soup):
         else:
             print(f"{i+1}. Ürün linki bulunamadı")
 
-
-
-
+# dış sayfalarda gezinmek
 page_number = 1
 while True:
     print(f'\n{page_number}. Sayfa')
     url = f'https://www.amazon.com.tr/s?k=laptop&page={page_number}'
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
-
+    
+    # scraping işlemi için fonksiyon çağırılır.
     scraping(soup)
-
+    
+    #buton yoksa dur artık. 
     next_button = soup.find('a', class_='s-pagination-next')
     if next_button is None:
         print("Sayfa Kalmadı!!")
         break
 
     page_number += 1
-
+# excele kaydet
 df = pd.DataFrame(excel_datas)
 df.to_excel("amazon_laptop_verileri.xlsx", index=False)
 print("\n Veriler Excel dosyasına kaydedildi: amazon_laptop_verileri.xlsx")
